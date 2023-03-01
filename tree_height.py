fd29dae1cd49d7f21edd3e7e4b802d9cbf0312a3
DaniilsJarmonovs <124714257+DaniilsJarmonovs@users.noreply.github.com>
2023-03-01 22:52:10 +0000

    V1.4

--- tree_height.py
+++ tree_height.py
@@ -5,23 +5,15 @@ from collections import namedtuple
 
 elem = namedtuple("elem", ["value", "depth", "visited"])
 
-#def compute_height(n, parents):
-    # Write this function
-    #max_height = 0
-
-    # Your code here
-    #return max_height
-
 def main():
     inp = input()
     text = ""
     count = 0
     if("I" in inp):
-        print("Now input data from keyboard")
         count = int(input())
         text = input()
     elif("F" in inp):
-        FName = input("Enter the name of the file: ")
+        FName = input()
         if("a" in FName):
             print("Sorry, the name can't contain letter 'a'")
         else:
@@ -38,7 +30,6 @@ def main():
     for i in range(count):
         val = l[i]
         if(not val.visited):
-            #print("Checked ", i)
             tail = [i]
             height = 1
             while(val.value != -1):
@@ -48,17 +39,14 @@ def main():
                 if(val.visited):
                     height = height + val.depth - 1
                     break
-            #print("Height is", height)
             if height>max_height:
                 max_height = height
-            #print(tail)
             n = val.depth
             for j in range(len(tail)-1, -1, -1):
                 node = tail[j]
                 if l[node].depth<n:
                     l[node] = elem(l[node].value, n, True)
                 n += 1
-    #print(l)
     print(max_height+1)
 
 
@@ -68,5 +56,3 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-#main()
-# print(numpy.array([1,2,3]))
